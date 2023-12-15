6a376505d756e3ab4dda0502aac8943253aa450b
Darshan Kalli <darshankoli2397@gmail.com>
2023-12-15 09:11:21 +0530

    code added

---
+++ Darshan-porfolio/PortfolioAPis/routes/userAccess/userRoleMapping.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userRoleMappingService = require('../../services/UserAccess/userRoleMapping');
+
+
+router
+.post('/get',userRoleMappingService.get)
+
+module.exports = router;
